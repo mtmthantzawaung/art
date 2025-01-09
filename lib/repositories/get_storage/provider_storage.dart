77f43e8eff57b303148009169edb81fb20282776
THANTZAWAUNG-PC\ThantZawAung <thantzawaung@metateammyanmar.com>
2025-01-09 10:57:04 +0630

    update

---
+++ lib/repositories/get_storage/provider_storage.dart
@@ -0,0 +1,25 @@
+import 'package:get_storage/get_storage.dart';
+
+class CurrentProviderSetting {
+  factory CurrentProviderSetting() {
+    return _instance;
+  }
+  CurrentProviderSetting._internal();
+
+  static final CurrentProviderSetting _instance =
+      CurrentProviderSetting._internal();
+
+  // key
+  static const String _providerIdKey = 'providerId';
+
+  Future<String?> get() async {
+    final box = GetStorage();
+    final providerId = box.read(_providerIdKey);
+    return providerId;
+  }
+
+  Future<void> update({required String providerId}) async {
+    final box = GetStorage();
+    await box.write(_providerIdKey, providerId);
+  }
+}
